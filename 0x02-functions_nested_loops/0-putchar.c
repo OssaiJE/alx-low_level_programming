0248404e3990a558c52c403f9fc1656f923ee9f8
Julius Ossai <OssaiJE1@gmail.com>
2022-07-05 16:43:31 -0700

    Updated C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+	int start;
+	char word[] = "_putchar";
+
+	for (start = 0; start < 8; start++)
+	{
+		_putchar(word[start]);
+	}
+	_putchar('\n');
+	return (0);
 }
